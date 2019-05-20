d85ec2a2f81849812abcd167f3a32682d3f98a28
Prokopenko Ivan <ivan.prokopenko@together.com>
2019-05-20 14:34:06 +0300

    Electron handling menu bars init

---
+++ index.js
@@ -0,0 +1,71 @@
+const electron = require("electron");
+
+const {app, BrowserWindow, Menu, ipcMain} = electron;
+
+let mainWindow;
+let addWindow;
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(`file://${__dirname}/main.html`);
+    mainWindow.on('closed', () => app.quit() );
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
+
+});
+function createAddWindow(){
+ addWindow = new BrowserWindow({
+     width: 300,
+     height: 200,
+     title: 'Add new Todo'
+ });
+ addWindow.loadURL(`file://${__dirname}/add.html`);
+ addWindow.on('closed', () => addWindow = null)
+}
+ipcMain.on('todo:add', (event, value) => {
+    mainWindow.webContents.send('todo:add',value);
+    console.log("value", value);
+    addWindow.close();
+});
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'New Todo',
+                click() {
+                    createAddWindow();
+                }
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+                click() {
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+if(process.platform === 'darwin'){
+    menuTemplate.unshift({});
+}
+//production
+//development
+//test
+//staging
+if(process.env.NODE_ENV !== "production"){
+    menuTemplate.push(
+        {   role:'reload'   },
+        {   label: 'View',
+            submenu: [{
+                label: "Toogle Developer Tools",
+                accelerator: process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+    })
+}
